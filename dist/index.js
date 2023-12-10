01ccbda8357e731e0190c96003572bfc946f177c
Hariharan S <41798115+Hariks17@users.noreply.github.com>
2023-12-10 20:27:44 +0000

    Add files via upload

---
+++ dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+let id = 5;
+let company = "hari";
+let isOwner = true;
+let age = 24;
+let ids = [1, 2, 3, 4, 5];
+let cid = 1;
+let customerId = cid;
+function addNumber(x, y) {
+    return x + y;
+}
---
+++ src/index.ts
@@ -0,0 +1,14 @@
+let id: number = 5;
+let company: string = "hari";
+let isOwner: boolean = true;
+let age: any = 24;
+
+let ids: number[] = [1, 2, 3, 4, 5];
+
+let cid: any = 1;
+
+let customerId = cid as number;
+
+function addNumber(x: number, y: number): number {
+  return x + y;
+}
